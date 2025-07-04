f36337441d907c6fd181f91eea2a780ea62dff16
Alexey Bobkov <alexeybobkov47@gmail.com>
2025-07-04 12:19:43 +0400

    hm2: implement CircularQueue

--- homework/slices_and_arrays/homework_test.go
+++ homework/slices_and_arrays/homework_test.go
@@ -9,42 +9,110 @@ import (
 
 // go test -v homework_test.go
 
-type CircularQueue struct {
-	values []int
-	// need to implement
+type allowed interface {
+	int | int8 | int16 | int32 | int64
+}
+type CircularQueue[T allowed] struct {
+	len    int
+	first  int
+	last   int
+	values []T
 }
 
-func NewCircularQueue(size int) CircularQueue {
-	return CircularQueue{} // need to implement
+// создать очередь с определенным размером буффера
+func NewCircularQueue[T allowed](size int) CircularQueue[T] {
+	return CircularQueue[T]{
+		len:    size,
+		first:  0,
+		last:   size - 1,
+		values: make([]T, size),
+	}
 }
 
-func (q *CircularQueue) Push(value int) bool {
-	return false // need to implement
+func (q *CircularQueue[T]) nextLast() {
+	next := q.last + 1
+	if next == q.len {
+		next = 0
+	}
+
+	q.last = next
+}
+func (q *CircularQueue[T]) nextFirst() {
+	next := q.first + 1
+	if next == q.len {
+		next = 0
+	}
+
+	q.first = next
 }
 
-func (q *CircularQueue) Pop() bool {
-	return false // need to implement
+// добавить значение в конец очереди (false, если очередь заполнена)
+func (q *CircularQueue[T]) Push(value T) bool {
+	if q.Full() {
+		return false
+	}
+
+	q.nextLast()
+	q.values[q.last] = value
+
+	return true
 }
 
-func (q *CircularQueue) Front() int {
-	return -1 // need to implement
+// удалить значение из начала очереди (false, если очередь пустая)
+func (q *CircularQueue[T]) Pop() bool {
+	if q.Empty() {
+		return false
+	}
+	q.values[q.first] = 0
+	q.nextFirst()
+
+	return true
 }
 
-func (q *CircularQueue) Back() int {
-	return -1 // need to implement
+// получить значение из начала очереди (-1, если очередь пустая)
+func (q *CircularQueue[T]) Front() T {
+	if q.Empty() {
+		return -1
+	}
+
+	return q.values[q.first]
 }
 
-func (q *CircularQueue) Empty() bool {
-	return false // need to implement
+// получить значение из конца очереди (-1, если очередь пустая)
+func (q *CircularQueue[T]) Back() T {
+	if q.Empty() {
+		return -1
+	}
+
+	return q.values[q.last]
 }
 
-func (q *CircularQueue) Full() bool {
-	return false // need to implement
+// проверить пустая ли очередь
+func (q *CircularQueue[T]) Empty() bool {
+	for _, v := range q.values {
+		if v != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+// проверить заполнена ли очередь
+func (q *CircularQueue[T]) Full() bool {
+	for _, v := range q.values {
+		if v == 0 {
+			return false
+		}
+	}
+
+	return true
+
 }
 
 func TestCircularQueue(t *testing.T) {
 	const queueSize = 3
-	queue := NewCircularQueue(queueSize)
+	queue := NewCircularQueue[int](queueSize)
 
 	assert.True(t, queue.Empty())
 	assert.False(t, queue.Full())
@@ -83,4 +151,22 @@ func TestCircularQueue(t *testing.T) {
 
 	assert.True(t, queue.Empty())
 	assert.False(t, queue.Full())
+
+	//
+
+	queue64 := NewCircularQueue[int64](3)
+	assert.True(t, queue64.Push(1))
+	assert.True(t, queue64.Push(2))
+	assert.True(t, queue64.Push(3))
+	assert.True(t, queue64.Full())
+	assert.Equal(t, int64(1), queue64.Front())
+	assert.True(t, queue64.Pop())
+	assert.Equal(t, int64(2), queue64.Front())
+	assert.True(t, queue64.Pop())
+	assert.Equal(t, int64(3), queue64.Front())
+	assert.True(t, queue64.Pop())
+	assert.Equal(t, int64(-1), queue64.Front())
+	assert.False(t, queue64.Pop())
+	assert.True(t, queue64.Empty())
+
 }
