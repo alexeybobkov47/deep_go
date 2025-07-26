4b0a43d3cb5e739fddc233f461bc8d9a54103b94
Alexey Bobkov <alexeybobkov47@gmail.com>
2025-07-26 19:21:05 +0400

    hm5: implement functions

--- homework/functions/homework_test.go
+++ homework/functions/homework_test.go
@@ -7,19 +7,54 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Map(data []int, action func(int) int) []int {
-	// need to implement
-	return nil
+/*
+1\. Реализуйте функцию `func Map(data []int, action func(int) int) []int`, которая принимает функцию `action` и срез данных `data`.
+ Функция `Map` должна применить функцию `action` к каждому элементу среза `data` и вернуть новый срез с результатами.
+
+2\. Реализуйте функцию `func Filter(data []int, action func(int) bool) []int`, которая принимает функцию `action` и срез данных `data`.
+ Функция `Filter` должна вернуть новый срез, содержащий только те элементы `data`, для которых функция `action` возвращает `true`.
+
+3\. Реализуйте функцию `func Reduce(data []int, initial int, action func(int, int) int) int`, которая принимает функцию `action` (функцию двух аргументов),
+ срез данных `data` и начальное значение `initial`. Функция `Reduce` должна применить функцию `action` к каждому элементу `data` и начальному значению `initial`, накапливая результат.
+
+*/
+
+func Map[T any](data []T, action func(T) T) []T {
+	if data == nil {
+		return nil
+	}
+
+	resp := make([]T, len(data))
+
+	for i, d := range data {
+		resp[i] = action(d)
+	}
+
+	return resp
 }
 
-func Filter(data []int, action func(int) bool) []int {
-	// need to implement
-	return nil
+func Filter[T any](data []T, action func(T) bool) []T {
+	if data == nil {
+		return nil
+	}
+
+	resp := make([]T, 0, len(data))
+
+	for _, d := range data {
+		if action(d) {
+			resp = append(resp, d)
+		}
+	}
+
+	return resp
 }
 
-func Reduce(data []int, initial int, action func(int, int) int) int {
-	// need to implement
-	return 0
+func Reduce[T any](data []T, initial T, action func(T, T) T) T {
+	for _, d := range data {
+		initial = action(d, initial)
+	}
+
+	return initial
 }
 
 func TestMap(t *testing.T) {
