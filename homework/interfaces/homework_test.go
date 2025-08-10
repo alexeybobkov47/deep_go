f41054cbf2872a5a9e63bca65b994e6c3ed522b7
Alexey Bobkov <alexeybobkov47@gmail.com>
2025-08-10 16:58:30 +0400

    hm7: implement di container

--- homework/interfaces/homework_test.go
+++ homework/interfaces/homework_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,21 +19,26 @@ type MessageService struct {
 }
 
 type Container struct {
-	// need to implement
+	types map[string]interface{}
 }
 
 func NewContainer() *Container {
-	// need to implement
-	return &Container{}
+	return &Container{
+		types: make(map[string]any),
+	}
 }
 
 func (c *Container) RegisterType(name string, constructor interface{}) {
-	// need to implement
+	c.types[name] = constructor
 }
 
 func (c *Container) Resolve(name string) (interface{}, error) {
-	// need to implement
-	return nil, nil
+	if _, ok := c.types[name]; !ok {
+		return nil, fmt.Errorf("not found: %s", name)
+	}
+
+	t := c.types[name].(func() interface{})
+	return t(), nil
 }
 
 func TestDIContainer(t *testing.T) {
