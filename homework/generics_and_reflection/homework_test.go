321f7b1a886622eb8ea8f9de194a0e9ca5f6ab2c
Alexey Bobkov <alexeybobkov47@gmail.com>
2025-08-31 12:38:33 +0400

    hm-9: implement Serialize() struct by tags

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
+
+/*
+В домашнем задании нужно реализовать сериализацию объекта структуры данных в .properties формат с использованием тегов структур и рефлексии.
+
+При сериализации данных в некоторых случаях могут возникнуть проблемы с пустыми значениями.
+Например, если у вас есть структура, в которой некоторые поля могут быть не заполнены, и вы сериализуете, то в результате получится объект с пустыми полями.
+Если это не является ожидаемым поведением, то в нашей реализации можно будет использовать тег omitempty, чтобы пропустить пустые поля при сериализации.
+
+Стуктура для сериализации в .properties формат (поподробнее с .properties форматом можно ознакомиться здесь https://ru.wikipedia.org/wiki/Properties):
+*/
 
 type Person struct {
 	Name    string `properties:"name"`
@@ -15,9 +28,33 @@ type Person struct {
 	Married bool   `properties:"married"`
 }
 
-func Serialize(person Person) string {
-	// need to implement
-	return ""
+func Serialize[T any](s T) string {
+	t := reflect.TypeOf(s)
+	v := reflect.ValueOf(s)
+	out := strings.Builder{}
+
+	fields := reflect.VisibleFields(t)
+	lines := make([]string, 0, len(fields))
+	for _, f := range fields {
+		properties, ok := f.Tag.Lookup("properties")
+		if !ok {
+			continue
+		}
+
+		value := v.FieldByIndex(f.Index)
+
+		tags := strings.Split(properties, ",")
+
+		if len(tags) > 1 && tags[1] == "omitempty" && value.IsZero() {
+			continue
+		}
+
+		lines = append(lines, fmt.Sprintf("%s=%v", tags[0], value))
+	}
+
+	out.WriteString(strings.Join(lines, "\n"))
+
+	return out.String()
 }
 
 func TestSerialization(t *testing.T) {
